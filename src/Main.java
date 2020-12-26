07ad65d7f56e37e165399d270e904525a30131f6
NetologyQA12 <dvb90@bk.ru>
2020-12-26 16:47:15 +0700

    edit Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
 public class Main {
+
+    public static void main(String[] args) {
+        boolean registered = true;
+
+        int balance = 100;
+
+        int percent = 1;
+
+        int amount = 1100;
+
+        int bonus = amount * percent / 100;
+
+        int limit = 1000;
+
+        if (amount >= limit) {
+            balance = balance + amount + bonus;
+        } else {
+            balance = balance + amount;
+        }
+
+        System.out.println(balance);
+    }
 }
